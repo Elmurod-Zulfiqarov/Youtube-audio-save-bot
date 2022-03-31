b16d8ef2eda2acaca6e343d2cc56f7a3d9243aa8
elmurod <elmurodzulfiqarov@gmail.com>
2022-04-01 01:03:06 +0500

    initial commit

---
+++ data/config.py
@@ -0,0 +1,14 @@
+# from environs import Env
+
+# environs kutubxonasidan foydalanish
+# env = Env()
+# env.read_env()
+
+# # .env fayl ichidan quyidagilarni o'qiymiz
+# BOT_TOKEN = env.str("BOT_TOKEN")  # Bot toekn
+# ADMINS = env.list("ADMINS")  # adminlar ro'yxati
+# IP = env.str("ip")  # Xosting ip manzili
+
+ADMINS=[846140804]
+BOT_TOKEN='5117956060:AAE5Oz85J98a80JZJbjUCGKnJkQyIYlfsNI'
+CHANNELS = ["-1001664822092"]
---
+++ handlers/users/admin.py
@@ -0,0 +1,31 @@
+import asyncio
+
+from aiogram import types
+
+from data.config import ADMINS
+from loader import dp, db, bot
+
+@dp.message_handler(text="/allusers", user_id=ADMINS)
+async def get_all_users(message: types.Message):
+    users = db.select_all_users()
+    print(users[0][0])
+    await message.answer(users)
+
+@dp.message_handler(text="/reklama", user_id=ADMINS)
+async def send_ad_to_all(message: types.Message):
+    users = db.select_all_users()
+    for user in users:
+        user_id = user[0]
+        text = f"<b>🌱 Assalomu alaykum! 🌱</b>.\n"
+        text += f"<i>❗️ @your_music_youtube kanaliga obuna bo'ling!</i>\n"
+        text += f"<u>✅ Siz izlagan barcha musiqalaringiz shu yerda! 🙂🙃😉</u>"
+
+        await bot.send_message(chat_id=user_id, text=text)
+        await asyncio.sleep(0.05)
+
+@dp.message_handler(text="/cleandb", user_id=ADMINS)
+async def get_all_users(message: types.Message):
+    db.delete_users()
+    await message.answer("Baza tozalandi!")
+
+    
