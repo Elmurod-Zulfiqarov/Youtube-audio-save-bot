7ebac41a38c6eba95a9c97fa30e468365b08865e
elmurod <elmurodzulfiqarov@gmail.com>
2022-04-20 18:00:50 +0500

    for heroku

--- data/config.py
+++ data/config.py
@@ -8,3 +8,7 @@
 # BOT_TOKEN = env.str("BOT_TOKEN")  # Bot toekn
 # ADMINS = env.list("ADMINS")  # adminlar ro'yxati
 # IP = env.str("ip")  # Xosting ip manzili
+
+ADMINS=[846140804]
+BOT_TOKEN='5322581627:AAFhhvFYTvsx7noMnPGSOf7GjFCYoGbc1iE'
+CHANNELS = ["-1001664822092"]
