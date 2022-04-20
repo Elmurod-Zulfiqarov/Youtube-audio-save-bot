31bdbde808ac9f06b89e5a30657e6a6e85795f35
elmurod <elmurodzulfiqarov@gmail.com>
2022-04-20 19:24:23 +0500

    for heroku

--- data/config.py
+++ data/config.py
@@ -1,11 +1,13 @@
 # from environs import Env
 
-# environs kutubxonasidan foydalanish
+# # environs kutubxonasidan foydalanish
 # env = Env()
 # env.read_env()
 
-# # .env fayl ichidan quyidagilarni o'qiymiz
+# # # .env fayl ichidan quyidagilarni o'qiymiz
 # BOT_TOKEN = env.str("BOT_TOKEN")  # Bot toekn
 # ADMINS = env.list("ADMINS")  # adminlar ro'yxati
 # IP = env.str("ip")  # Xosting ip manzili
-
+CHANNELS = [-1001664822092]
+ADMINS=[846140804]
+BOT_TOKEN='5117956060:AAHsLHhoocP-pNlUl2qkncfMbVSBbnLEeRo'
